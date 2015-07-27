b244779407509965fe023c707f73245787195eb8
Robert <gevent@CSSI2015-14.local>
2015-07-27 09:36:07 -0400

    Second try at Twitter API

--- feedlytwitter/main.py
+++ feedlytwitter/main.py
@@ -15,10 +15,33 @@
 # limitations under the License.
 #
 import webapp2
+import json
+import logging
+import oauth2
+import requests
+import requests_oauthlib
+import tweepy
+
+
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        # user = api.get_user('SiriusArtistry')
+        # print user.screen_name
+        # print user.followers_count
+        # for friend in user.friends():
+        #    print friend.screen_name
+        consumer_key = 'kCh51IOR8BVdmW3Iuf0TpjYD6'
+        consumer_secret = 'p4QsgwRFvBMa9wEMkwpzYRFEPwYc4znupLd4xlBWlFLYZLjOpf'
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        # auth.set_access_token(access_token, access_token_secret)
+
+        api = tweepy.API(auth)
+
+        public_tweets = api.home_timeline()
+        for tweet in public_tweets:
+            print tweet.text
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
