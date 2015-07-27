8753ebe52cf0840a2fb76e8fb4b497efa60fc514
gEvent <gevent@CSSI2015-4.local>
2015-07-27 16:17:26 -0400

    add googleplus

--- feedlytwitter/main.py
+++ feedlytwitter/main.py
@@ -14,12 +14,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import jinja2
+import os
 import webapp2
+from google.appengine.api import users
+import json
+from google.appengine.api import urlfetch
+
+
+jinja_environment = jinja2.Environment(
+  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('mainpage.html')
+        self.response.out.write(template.render())
+        data = urlfetch.fetch('''https://www.googleapis.com/plus/v1/people/100600334103193921902?key=AIzaSyDyYgPPyl7FBES41CbPcJ6Af1CM7tp8Cqc''').content
+
+
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
 app = webapp2.WSGIApplication([
+    ('/login', LoginHandler),
     ('/', MainHandler)
 ], debug=True)
