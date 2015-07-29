4f4b04ffd338779202649a657796a3b377c08d5a
Robert <gevent@CSSI2015-14.local>
2015-07-28 22:21:15 -0400

    Login with Twitter OR Facebook working

--- everything/main.py
+++ everything/main.py
@@ -53,7 +53,7 @@ class Login(webapp2.RequestHandler):
             self.redirect('/')
 
 class Home(webapp2.RequestHandler):
-    def get(self):
+    def any(self):
         logging.info("home (get) handler")
         # Retrieve values from cookies.
         serialized_credentials = self.request.cookies.get('credentials')
@@ -99,8 +99,133 @@ class Home(webapp2.RequestHandler):
             template = jinja_environment.get_template('templates/login.html')
             self.response.write(template.render())
             return
+        logging.info("PROVIDER NAME IS: %s", credentials.provider_name)
+
+            #TWITTER -----------------------------
+        provider_name = credentials.provider_name
+        logging.info("action handler got provider_name %s", provider_name)
+        result = authomatic.login(Webapp2Adapter(self), provider_name)
+        logging.info('RESULT IS: %s' % result)
+        serialized_credentials = self.request.cookies.get('credentials')
+        user_id = self.request.cookies.get('user_id')
+
+        response = authomatic.access(serialized_credentials,
+                                     url='https://api.twitter.com/1.1/statuses/home_timeline.json',
+                                     method='GET')
+        logging.info('RESPONSE IS: %s' % response)
+        tweets = []
+        fstatuses = []
+        if result:
+            # If there is result, the login procedure is over and we can write to response.
+            self.response.write('<a href="..">Home</a>')
+
+            if result.error:
+                # Login procedure finished with an error.
+                self.response.write(u'<h2>Damn that error: {}</h2>'.format(result.error.message))
 
 
+            elif result.user:
+                # Hooray, we have the user!
+
+                # OAuth 2.0 and OAuth 1.0a provide only limited user data on login,
+                # We need to update the user to get more info.
+                if not (result.user.name and result.user.id):
+                    result.user.update()
+
+                # Welcome the user.
+                # self.response.write(u'<h1>Hi {}</h1>'.format(result.user.name))
+                # self.response.write(u'<h2>Your id is: {}</h2>'.format(result.user.id))
+                # self.response.write(u'<h2>Your email is: {}</h2>'.format(result.user.email))
+                logging.info('CREDENTIALS ARE: %s' % result.user.credentials)
+                credentials = result.user.credentials
+                logging.info('CREDENTIALS VARIABLE VALUE IS: %s' % credentials)
+                # self.response.set_cookie('credentials', credentials)
+
+
+                # Seems like we're done, but there's more we can do...
+
+                # If there are credentials (only by AuthorizationProvider),
+                # we can _access user's protected resources.
+                if result.user.credentials:
+
+                    # Each provider has it's specific API.
+                    if result.provider.name == 'fb':
+                        # We will access the user's 5 most recent statuses.
+                        urlf = 'https://graph.facebook.com/{}?fields=feed.limit(10)'
+                        urlf = urlf.format(result.user.id)
+                        logging.info("USER ID IS: %s" % result.user.id)
+
+                        # Access user's protected resource.
+                        response = result.provider.access(urlf)
+
+                        if response.status == 200:
+                            # Parse response.
+                            statuses = response.data.get('feed').get('data')
+                            error = response.data.get('error')
+
+                            if error:
+                                self.response.write(u'Damn that error: {}!'.format(error))
+                            elif statuses:
+                                fstatuses = []
+                                for message in statuses:
+                                    s = {}
+                                    s['text'] = message.get('message')
+                                    s['date'] = message.get('created_time')
+                                    fstatuses.append(s)
+
+                                    text = message.get('message')
+                                    date = message.get('created_time')
+                        else:
+                            self.response.write('Damn that unknown error!<br />')
+                            self.response.write(u'Status: {}'.format(response.status))
+
+                    if result.provider.name == 'tw':
+                        # We will get the user's 5 most recent tweets.
+                        urlt = 'https://api.twitter.com/1.1/statuses/home_timeline.json'
+
+                        # You can pass a dictionary of querystring parameters.
+                        response = result.provider.access(urlt, {'count': 10})
+                        logging.info('RESPONSE IS: %s' % response)
+
+                        # Parse response.
+                        if response.status == 200:
+                            logging.info('RESPONSE STATUS IS A-OKAY!')
+                            if type(response.data) is list:
+                                # Twitter returns the tweets as a JSON list.
+                                # self.response.write('Your 5 most recent tweets:')
+                                # logging.info('RESPONSE DATA IS: %s' % response.data)
+                                tweets = []
+                                for tweet in response.data:
+                                    t = {}
+                                    t['text'] = tweet.get('text')
+                                    t['date'] = tweet.get('created_at')
+                                    t['name'] = tweet.get('user').get('name')
+                                    # t['media'] = tweet.get('mediaurl')
+                                    # logging.info('MEDIA URL IS: %s' % media)
+                                    t['handle'] = tweet.get('user').get('screen_name')
+
+                                    tweets.append(t)
+
+                                    # self.response.write(format(name, handle))
+                                    # self.response.write(format(text.replace(u'\u2013', '[???]')))
+                                    # self.response.write(format(date))
+
+                                    #PUT THESE THREE ENTIRES IN A DICT SO THAT YOU CAN PRINT IT OUT IN HTML
+
+                            elif response.data.get('errors'):
+                                self.response.write(u'Damn that error: {}!'.\
+                                                    format(response.data.get('errors')))
+                        else:
+                            self.response.write('Damn that unknown error!<br />')
+                            self.response.write(u'Status: {}'.format(response.status))
+
+                    if result.provider.name == 'go':
+                        pass
+
+
+
+
+            # NEWS ------------------------------
         url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?"
         api_key ="api-key=7169254a2f887db9ab1c3c629fed79d3:16:72574373"
         nyt_data_source = urlfetch.fetch(url+api_key)
@@ -124,7 +249,7 @@ class Home(webapp2.RequestHandler):
             articles.append(a)
 
 
-
+        logging.info('STATUSES ARE: %s', fstatuses)
 
         # Create links to the Login handler.
         template = jinja_environment.get_template('templates/mainpage.html')
@@ -132,14 +257,17 @@ class Home(webapp2.RequestHandler):
                                             'articles' : articles,
                                             'name' : format(user_name),
                                             'provider' : format(dict(fb='Facebook', tw='Twitter')[credentials.provider_name]),
-                                            'providerslug' : format(credentials.provider_name)
+                                            'providerslug' : format(credentials.provider_name),
+                                            'tweets' : tweets,
+                                            'statuses' : fstatuses
+
                                         }))
 
 
 class Refresh(webapp2.RequestHandler):
     def get(self):
         logging.info("refresh (get) handler")
-        self.response.write('<a href="..">Home</a>')
+        self.response.write('<a href="..">Home</a><br>')
 
         serialized_credentials = self.request.cookies.get('credentials')
         credentials = authomatic.credentials(serialized_credentials)
@@ -174,7 +302,6 @@ class Action(webapp2.RequestHandler):
     def any(self, provider_name):
         logging.info("action handler got provider_name %s", provider_name)
         result = authomatic.login(Webapp2Adapter(self), provider_name)
-        # result = self.request.cookies.get('result')
         logging.info('RESULT IS: %s' % result)
         serialized_credentials = self.request.cookies.get('credentials')
         user_id = self.request.cookies.get('user_id')
@@ -363,36 +490,36 @@ class Logout(webapp2.RequestHandler):
         # Redirect home.
         self.redirect('./')
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        logging.info("main handler (get)")
-        #template = jinja_environment.get_template('templates/news.html')
-        #url: http://api.nytimes.com/svc/search/v2/articlesearch.response-format?[q=search term&fq=filter-field:(filter-term)&additional-params=values]&api-key=####
-        #base_url = "http://api.nytimes.com/svc/search/v2/articlesearch.response-format?q=sports"
-        #search_term =
-        url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?"
-        api_key ="api-key=7169254a2f887db9ab1c3c629fed79d3:16:72574373"
-        nyt_data_source = urlfetch.fetch(url+api_key)
-        nyt_json_content = nyt_data_source.content
-        parsed_nyt_dictionary = json.loads(nyt_json_content)
-        #not all articles have these 3 things... what do we do?
-        articles = []
-        for doc in parsed_nyt_dictionary['response']['docs']:
-            a = {}
-            art_headline = doc.get('headline').get('main')
-            if art_headline == None:
-                 art_headline = doc.get('headline').get('print_headline')
-            if art_headline == None:
-                 art_headline = ''
-            a['headline'] = art_headline
-            art_lead_para = doc.get('lead_paragraph')
-            if art_lead_para == None:
-                art_lead_para = ''
-            a['lead_paragraph'] = art_lead_para
-            a['web_url'] = doc.get('web_url')
-            articles.append(a)
-        template = jinja_environment.get_template('templates/mainpage.html')
-        self.response.write(template.render({'articles' : articles}))
+# class MainHandler(webapp2.RequestHandler):
+#     def get(self):
+#         logging.info("main handler (get)")
+#         #template = jinja_environment.get_template('templates/news.html')
+#         #url: http://api.nytimes.com/svc/search/v2/articlesearch.response-format?[q=search term&fq=filter-field:(filter-term)&additional-params=values]&api-key=####
+#         #base_url = "http://api.nytimes.com/svc/search/v2/articlesearch.response-format?q=sports"
+#         #search_term =
+#         url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?"
+#         api_key ="api-key=7169254a2f887db9ab1c3c629fed79d3:16:72574373"
+#         nyt_data_source = urlfetch.fetch(url+api_key)
+#         nyt_json_content = nyt_data_source.content
+#         parsed_nyt_dictionary = json.loads(nyt_json_content)
+#         #not all articles have these 3 things... what do we do?
+#         articles = []
+#         for doc in parsed_nyt_dictionary['response']['docs']:
+#             a = {}
+#             art_headline = doc.get('headline').get('main')
+#             if art_headline == None:
+#                  art_headline = doc.get('headline').get('print_headline')
+#             if art_headline == None:
+#                  art_headline = ''
+#             a['headline'] = art_headline
+#             art_lead_para = doc.get('lead_paragraph')
+#             if art_lead_para == None:
+#                 art_lead_para = ''
+#             a['lead_paragraph'] = art_lead_para
+#             a['web_url'] = doc.get('web_url')
+#             articles.append(a)
+#         template = jinja_environment.get_template('templates/mainpage.html')
+#         self.response.write(template.render({'articles' : articles}))
 
 class TrialHandler(webapp2.RequestHandler):
     def get(self):
@@ -407,7 +534,7 @@ ROUTES = [webapp2.Route(r'/login/<:.*>', Login, handler_method='any'),
           webapp2.Route(r'/refresh', Refresh),
           webapp2.Route(r'/action/<:.*>', Action, handler_method='any'),
           webapp2.Route(r'/logout', Logout),
-          webapp2.Route(r'/', Home)]
+          webapp2.Route(r'/', Home, handler_method='any')]
 
 # Instantiate the WSGI application.
 app = webapp2.WSGIApplication(ROUTES, debug=True)
