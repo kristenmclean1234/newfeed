d8541f7744d7684a9a111272bea5e0e8eeb5eea5
gEvent <gevent@CSSI2015-4.local>
2015-07-30 11:06:53 -0400

    search bar hide and show

--- everything/javascript/javascript.js
+++ everything/javascript/javascript.js
@@ -1,22 +1,27 @@
 $(document).ready(function(){
+        $('#searchbar').hide();
     $("#facebook").click(function(){
         $(".TwitterPost").hide();
         $(".NewsPost").hide();
         $(".FacebookPost").show();
+        $('#searchbar').hide();
     });
     $("#twitter").click(function(){
         $(".FacebookPost").hide();
         $(".NewsPost").hide();
         $(".TwitterPost").show();
+        $('#searchbar').hide();
     });
     $("#news").click(function(){
         $(".FacebookPost").hide();
         $(".TwitterPost").hide();
         $(".NewsPost").show();
+        $('#searchbar').show();
     });
     $("#home").click(function(){
         $(".FacebookPost").show();
         $(".TwitterPost").show();
         $(".NewsPost").show();
+        $('#searchbar').hide();
     });
 });
